e464a2d1f1e6bebda91942d70b01938c33b4959a
IT20648190 <it20648190@my.sliit.lk>
2023-04-27 22:19:45 +0530

    changes in structure

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -14,14 +14,14 @@ const cartRouter = express.Router();
 cartRouter.route("/").post(protect, createCart);
 cartRouter
   .route("/:userId")
-  .get(protect, getCartByUserId)
-  .put(protect, updateCart)
-  .delete(protect, deleteCart);
-cartRouter.route('/:userId/:productId').delete(protect, deleteProductFromCart);
+  .get(protectUser, getCartByUserId)
+  .put(protectUser, updateCart)
+  .delete(protectUser, deleteCart);
+cartRouter.route('/:userId/:productId').delete(protectUser, deleteProductFromCart);
 // cartRouter.route('/:userId/:productId/').put(protect, updateProductQuantity);
-cartRouter.route('/getTotalPrice/:userId').get(protect,buyerProtect, getTotalPrice);
+cartRouter.route('/getTotalPrice/:userId').get(protectUser,buyerProtect, getTotalPrice);
 cartRouter
   .route("/getCartCount/:userId")
-  .get(protect, buyerProtect, getCartCount);
+  .get(protectUser, buyerProtect, getCartCount);
 
 export default cartRouter;
